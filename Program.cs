6615b5c522ee7489fc5d6089881f6078e8012047
alfahrelrifananda <pahrel1234@gmail.com>
2025-09-01 13:33:51 +0700

    new

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 public class Program
 {
     public static void Main(String[] args)
     {
+        // ! 1.
         // * * Console.Write : Displaying output
         /*
         Console.WriteLine("This");
@@ -14,6 +17,27 @@ public class Program
         // * * Console.Write : Outputing sound
         /*
         Console.Beep();
+        */
+
+        // ! 2.
+        // ** Comment and escape sequence
+        /*
+        Console.Write("Hey!");
+        Console.WriteLine("Hello");
+        */
+
+        // This is a comment
+        /*
+        This
+        is
+        a
+        multiline
+        comment
+        */
+
+        // ** Escape Squence
+        /*
+        Console.WriteLine("\t This is would have a tab");
         */
     }
 }
