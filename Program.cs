4f46c4b6e22ee80a50c63c38a6debb4dae51f74d
alfahrelrifananda <pahrel1234@gmail.com>
2025-09-01 12:53:25 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        // * * Console.Write : Displaying output
+        /*
+        Console.WriteLine("This");
+        Console.WriteLine("is");
+        Console.WriteLine("Very");
+        Console.WriteLine("Nice");
+        */
+
+        // * * Console.Write : Outputing sound
+        /*
+        Console.Beep();
+        */
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Personal-cSharp-full-course
